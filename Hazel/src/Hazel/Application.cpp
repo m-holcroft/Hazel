9b0883bf6d60fd2201d73b95d59b8c2e143cdce7
Mathew Holcroft <matthew.holcroft@drspclaims.co.uk>
2019-11-25 12:42:16 +0000

    Set up basic app and entry point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	Hazel::Application::Application() {
+
+	}
+
+	Hazel::Application::~Application() {
+
+	}
+
+	void Hazel::Application::Run() {
+		while (true);
+	}
+}
+
